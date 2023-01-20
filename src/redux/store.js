11394cb709dfd4be2d7cf4fa2612debc8d2a6635
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2023-01-20 23:30:16 +0200

    up

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { contactsReducer } from './contactsSlice/contactsSlice';
 import { filterReducer } from './filterSlice/filterSlice';
 
+const persistConfig = {
+  key: 'contacts',
+  storage: storage,
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedContactsReducer,
     filter: filterReducer,
   },
 });
+
+export const persistor = persistStore(store);
